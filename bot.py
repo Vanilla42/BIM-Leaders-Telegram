55fe3e1b1f36878abcb0a585054ac133e99d9932
Vanilla42 <72744472+Vanilla42@users.noreply.github.com>
2022-03-03 16:41:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,21 +9,15 @@ logging.BasicConfig(level=logging.INFO)
 bot = Bot(token = config.TOKEN)
 dp = Dispatcher(bot)
 
+# Start and help
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+  await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
 # Echo
 @dp.message_handler()
 async def echo(message: types.Message):
   await message.answer(message.text)
- 
-def main():
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-    
-    # Run long-polling
-    executor.start_polling(dp, skip_updates=True)
 
 if __name__ == '__main__':
-    main()
+    executor.start_polling(dp, skip_updates=True)
