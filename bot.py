ecf0205d06e7c6375fc1546b36f5e5572506a3c1
Vanilla42 <72744472+Vanilla42@users.noreply.github.com>
2022-03-03 17:14:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,22 +2,40 @@ import config
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
-# Log level
+# Log
 logging.BasicConfig(level=logging.INFO)
 
-# Bot init
-bot = Bot(token = config.TOKEN)
-dp = Dispatcher(bot)
-
-# Start and help
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-  await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
-
-# Echo
-@dp.message_handler()
+# Handlers
+async def start(message: types.Message):
+  await message.reply("Hello, {0}".format(message.from_user.first_name))
+#@dp.message_handler()
 async def echo(message: types.Message):
   await message.answer(message.text)
+
+# Lambda functions
+async def register_handlers(dp: Dispatcher):
+  dp.register_message_handler(start, commands=["start"])
+  dp.register_message_handler(echo)
+
+async def process_event(update, dp:Dispatcher):
+  Bot.set_current(dp.bot)
+  await dp.process_update(update)
+
+# Serverless entry point
+async def main(**kwargs):
+  bot = Bot(token = config.TOKEN)
+  dp = Dispatcher(bot)
+  
+  await register_handlers(dp)
+  
+  update = types.Update.to_object(kwargs)
+  await process_event(update, dp)
+  
+  return "Ok"
+
+# Bot init
+#bot = Bot(token = config.TOKEN)
+#dp = Dispatcher(bot)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
