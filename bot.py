c5fc75d1b50d6dbf1639b0e5298339bd32b77507
Vanilla42 <72744472+Vanilla42@users.noreply.github.com>
2022-03-09 09:07:59 +0200

    telebot

--- bot.py
+++ bot.py
@@ -1,41 +1,89 @@
-import config
-import logging
-from aiogram import Bot, Dispatcher, executor, types
+import os
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import time
+import datetime
 
-# Log
-logging.BasicConfig(level=logging.INFO)
 
-# Handlers
-async def start(message: types.Message):
-  await message.reply("Hello, {0}".format(message.from_user.first_name))
-#@dp.message_handler()
-async def echo(message: types.Message):
-  await message.answer(message.text)
+API_KEY = os.environ['TOKEN']
+bot = telebot.TeleBot(API_KEY)
 
-# Lambda functions
-async def register_handlers(dp: Dispatcher):
-  dp.register_message_handler(start, commands=["start"])
-  dp.register_message_handler(echo)
 
-async def process_event(update, dp:Dispatcher):
-  Bot.set_current(dp.bot)
-  await dp.process_update(update)
+day = datetime.datetime.today().weekday()
 
-# Serverless entry point
-async def main(**kwargs):
-  bot = Bot(token = "5249729175:AAHfTrJdQhdokgTUAIxPsAlRkEUlxmVUnAk")
-  dp = Dispatcher(bot)
+def get_time():
+  hour = datetime.datetime.now().hour + 2
+  t = 0         # Morning  0-10
+  if hour > 10: # Day     10-15
+    t = 1
+  if hour > 15: # Evening 15-24
+    t = 2
+  return t
+
+kitchen_workers = [
+  [["Денис", "Альбіна"],["Назар", "Маша", "Артем"],["Слава", "Денис"]],
+  [["Слава", "Артем"],["Авраменко Іван", "Олександр", "Альбіна"],["Назар", "Артем"]],
+  [["Назар", "Денис"],["Маша", "Стас", "Слава"],["Альбіна", "Авраменко Іван"]],
+  [["Авраменко Іван", "Олександр"],["Артем", "Денис", "Альбіна"],["Стас", "Назар"]],
+  [["Маша", "Стас"],["Слава", "Альбіна", "Назар"],["Денис", "Артем"]],
+  [["Савіна Юлія", "Тюріна Тіна"],["Денис", "Романюк Катерина", "Ганін Петро"],["Маша", "Цвігун Людмила"]],
+  [["Зотіна Анастасія", "Авраменко Іван"],["Ткаченко Вікторія", "Слава", "Шавловська Яна"],["Наголюк Євген", "Авраменко Антоніна"]]
+]
+kitchen_heplers = [
+  [["Півень Денис", "Авраменко Антоніна"],["Редчиць Денис", "Тюріна Тіна"],["Проскурня Михайло", "Зотіна Анастасія"]],
+  [["Налогюк Євген", "Цвігун Людмила"],["Пінчук Іван", "Авраменко Іван"],["Романюк Катерина", "Чаур Анастасія"]],
+  [["Ганін Петро", "Редчиць Денис"],["Ткаченко Вікторія", "Проскурня Михайло"],["Півень Денис", "Савіна Юлія"]],
+  [["Шавловська Яна", "Романюк Катерина"],["Савіна Юлія", "Алєксєєв Еміль"],["Цвігун Людмила", "Редчиць Денис"]],
+  [["Стас", "Чаур Анастасія"],["Півень Денис", "Зотіна Анастасія"],["Тюріна Тіна", "Алєксєєв Еміль"]],
+  [["Назар", "Альбіна"],["Авраменко Іван", "Олександр"],["Авраменко Іван", "Ткаченко Вікторія"]],
+  [["Пінчук Іван", "Алєксєєв Еміль"],["Авраменко Антоніна", "Чаур Анастасія"],["Проскурня Михайло", "Шавловська Яна"]]
+]
+
+def message_kitchen():
+  time = get_time()
+  timetxt = ""
+  if time == 0:
+    timetxt = "\U000023F0 *MORNING* \U000023F0"
+  if time == 1:
+    timetxt = "\U00002600 *DAY* \U00002600"
+  if time == 2:
+    timetxt = "\U0001F319 *EVENING* \U0001F319"
+  workers = "\n".join(kitchen_workers[day][time])
+  heplers = "\n".join(kitchen_heplers[day][time])
+  message = timetxt + "\n\U0001F373 KITCHEN:\n" + workers + "\n" + "\U0001F37D HELP: \n" + heplers
+  return message
+
+@bot.message_handler(commands=['kitchen'])
+def kitchen(message):
+  chat_id = message.chat.id
+  bot.delete_message(chat_id, message.message_id)
+  #message_bot_kitchen = bot.send_message(message.chat.id, message_kitchen(), reply_markup=markup_delete())
+  message_bot_kitchen = bot.send_message(chat_id, message_kitchen(), parse_mode 
+ = "MarkdownV2", disable_notification=True)
   
-  await register_handlers(dp)
+  time.sleep(10)
+  bot.delete_message(chat_id, message_bot_kitchen.message_id) 
   
-  update = types.Update.to_object(kwargs)
-  await process_event(update, dp)
-  
-  return "Ok"
 
-# Bot init
-#bot = Bot(token = config.TOKEN)
-#dp = Dispatcher(bot)
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+"""
+def gen_markup():
+  markup = InlineKeyboardMarkup()
+  markup.row_width = 2
+  markup.add(InlineKeyboardButton("Kitchen", callback_data="cb_yes"), InlineKeyboardButton("No", callback_data="cb_no"))
+  return markup
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+  if call.data == "cb_yes":
+    bot.answer_callback_query(call.id, "Answer is Yes")
+  elif call.data == "cb_no":
+    bot.answer_callback_query(call.id, "Answer is No")
+
+@bot.message_handler(func=lambda message: True)
+def message_handler(message):
+    bot.send_message(message.chat.id, "Yes/no?", reply_markup=gen_markup())
+    #bot.reply_to(message, message_kitchen())
+"""
+
+bot.infinity_polling()
